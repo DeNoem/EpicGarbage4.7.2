1a2caf4d026ad7c4f15d6f30cb7ca2e421ca76a1
Andrew Lobov <lobzan@mail.ru>
2022-01-14 21:36:18 +0300

    I broke your code

--- EpicGarbage4.7.2/3 task.Designer.cs
+++ EpicGarbage4.7.2/3 task.Designer.cs
@@ -42,9 +42,10 @@ namespace EpicGarbage4._7._2
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(175, 12);
+            this.label2.Location = new System.Drawing.Point(131, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.Size = new System.Drawing.Size(83, 13);
             this.label2.TabIndex = 13;
             this.label2.Text = "Конец периода";
             // 
@@ -64,18 +65,19 @@ namespace EpicGarbage4._7._2
             "10",
             "11",
             "12"});
-            this.comboBox2.Location = new System.Drawing.Point(175, 30);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Location = new System.Drawing.Point(131, 24);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(151, 24);
+            this.comboBox2.Size = new System.Drawing.Size(114, 21);
             this.comboBox2.TabIndex = 12;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 15;
             this.label1.Text = "Начало периода";
             // 
@@ -95,40 +97,41 @@ namespace EpicGarbage4._7._2
             "10",
             "11",
             "12"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 30);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(9, 24);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(151, 24);
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
             this.comboBox1.TabIndex = 14;
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Items.AddRange(new object[] {
+            "коммунальный",
             "индустриальный",
-            "строительный",
-            "коммунальный"});
-            this.comboBox3.Location = new System.Drawing.Point(332, 30);
-            this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            "строительный"});
+            this.comboBox3.Location = new System.Drawing.Point(249, 24);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(151, 24);
+            this.comboBox3.Size = new System.Drawing.Size(114, 21);
             this.comboBox3.TabIndex = 17;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(332, 12);
+            this.label3.Location = new System.Drawing.Point(249, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 16;
             this.label3.Text = "Тип мусора";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 66);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(9, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.Size = new System.Drawing.Size(113, 19);
             this.button1.TabIndex = 18;
             this.button1.Text = "Вывести результат";
             this.button1.UseVisualStyleBackColor = true;
@@ -137,18 +140,17 @@ namespace EpicGarbage4._7._2
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(176, 66);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Location = new System.Drawing.Point(132, 54);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(307, 132);
+            this.listBox1.Size = new System.Drawing.Size(231, 108);
             this.listBox1.TabIndex = 19;
             // 
             // _3_task
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 300);
+            this.ClientSize = new System.Drawing.Size(506, 244);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox3);
@@ -157,7 +159,7 @@ namespace EpicGarbage4._7._2
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox2);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "_3_task";
             this.Text = "_3_task";
             this.ResumeLayout(false);
